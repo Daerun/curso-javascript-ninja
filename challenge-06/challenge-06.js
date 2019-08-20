e8fa21c83c582e4cfd6500d50ff8b53664bea00b
Alan_Lima <alanlima2303@gmail.com>
2019-08-20 20:37:27 -0300

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,16 +4,14 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
-
+console.log( championship );
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
 Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
-
+var teams = ['Fortaleza', 'Ceara', 'Ferroviario', 'Guarani'];
 console.log( 'Times que estão participando do campeonato:', teams );
 
 /*
